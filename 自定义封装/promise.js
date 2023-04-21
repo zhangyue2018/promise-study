e437c548f903a30a75ba0c7cc5e388e96290c146
zhangyue <zhangyuea@hualala.com>
2023-04-21 16:53:03 +0800

    自定义Promise--状态只能更改一次

--- 自定义封装/promise.js
+++ 自定义封装/promise.js
@@ -8,6 +8,8 @@ function Promise(executor) {
 
     // resolve函数
     function resolve(data) {
+        // 判断状态--状态只能修改一次
+        if(self.promiseState !== 'pendding') return;
         // 1.修改对象的状态
         self.promiseState = 'fulfilled';
         // 2.修改对象结果值
@@ -16,6 +18,8 @@ function Promise(executor) {
 
     // reject函数
     function reject(data) {
+        // 判断状态--状态只能修改一次
+        if(self.promiseState !== 'pendding') return;
         // 1.修改对象的状态
         self.promiseState = 'rejected';
         // 2.修改对象结果值
